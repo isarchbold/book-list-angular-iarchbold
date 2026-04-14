dee5e14c566317f060973aa25e7d59c48ca0b580
isarchbold <i.archbold@uniandes.edu.co>
2026-04-13 23:20:23 -0500

    Arreglo para que funione

--- src/app/book/book-list/book-list.component.ts
+++ src/app/book/book-list/book-list.component.ts
@@ -12,11 +12,14 @@ export class BookListComponent implements OnInit {
   books: Array<Book> = [];
 
   constructor(private bookService: BookService) {}
+
   getBooks(): void {
     this.bookService.getBooks().subscribe((books) => {
       this.books = books;
     });
   }
+
   ngOnInit(): void {
-    this.getBooks();}
+    this.getBooks();
+  }
 }
